f0eaedc62010dcb8ee36e482073ef61f52ee316e
Kaia <kbergmanndean@gmail.com>
2021-05-19 14:31:22 -0400

    commit tree ddbdcb6eae7bc5c9fcb0f4a636922ddedcf491b4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+
+function findMatching(array,string) {
+    let newArray = array.filter(function(driver){ return driver.toUpperCase() === string.toUpperCase()})
+    return newArray
+}
+
+function fuzzyMatch(array,string) {
+    let newArray =array.filter(function(driver){ return driver[0]===string[0]})
+    return newArray
+}
+
+function matchName(array,string) {
+    let newArray =array.filter(function(driver){return driver.name===string})
+    return newArray
+}
